65aa6099dd0fcd2f5b72a0f88340fe69e5caaf89
codingcyber <vivek@codingcyber.com>
2020-02-12 14:05:34 +0530

    6-3 Creating Admin Model & Methods

---
+++ application/models/Admin_model.php
@@ -0,0 +1,32 @@
+<?php
+
+class Admin_model extends CI_Model {
+	// Category
+	public function insertCategory(){
+
+	}
+
+	public function selectCategories(){
+		
+	}
+
+	public function selectCategory(){
+		
+	}
+
+	public function updateCategory(){
+		
+	}
+
+	public function deleteCategory(){
+		
+	}
+
+	// Posts
+
+	// Pages
+
+	// Users
+
+	// Widgets
+}
